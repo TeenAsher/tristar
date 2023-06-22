3b598a1228c0d032a310e33960d1c2639f6b59d5
TeenAsher <>
2023-06-21 19:22:57 -0500

    Final 3

--- tristar-app.py
+++ tristar-app.py
@@ -10,13 +10,13 @@ from wtforms import StringField, IntegerField, DateField, EmailField, TelField, 
 
 app = Flask(__name__)
 
-GMAIL_PASSWORD = os.getenv('GMAIL_PASSWORD')
+GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'tanyushashilina1@gmail.com'
+app.config['MAIL_USERNAME'] = 'tatianashilina.usa@gmail.com'
 app.config['MAIL_PASSWORD'] = GMAIL_PASSWORD
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
@@ -30,7 +30,7 @@ class ReservationForm(FlaskForm):
     arrival = DateField('Select the date of your arrival:*', validators=[validators.InputRequired()])
     departure = DateField('Select the date of your departure:*', validators=[validators.InputRequired()])
     adults = IntegerField('Adults:*', validators=[validators.InputRequired(), validators.NumberRange(min=1, max=30)])
-    children = IntegerField('Children:*', validators=[validators.InputRequired(), validators.NumberRange(min=0, max=30)])
+    children = IntegerField('Children (please, type 0 if you will not have any):*', validators=[validators.InputRequired(), validators.NumberRange(min=0, max=30)])
     pets = RadioField('Will you have any pets?*', choices=['Yes', 'No'], validators=[validators.InputRequired()])
     room_type = RadioField('What type of room will you need?*', choices=['Single Queen', 'Single King', 'Double Queen'],
                             validators=[validators.InputRequired()])
@@ -55,7 +55,7 @@ def contacts():
 def reservation():
     form = ReservationForm()
     if form.validate_on_submit():
-        msg = Message('Reservation request', sender='tanyushashilina1@gmail.com', recipients=['tanyushashilina1@gmail.com'])
+        msg = Message('Reservation request', sender='tatianashilina.usa@gmail.com', recipients=['tatianashilina.usa@gmail.com'])
         msg.body = (f'You got a new reservation request. Here is the guest data:\n'
                    f'Name: {form.guest_name.data}\nEmail: {form.guest_email.data}\nPhone number: {form.guest_phone.data}\n'
                    f'Arrival: {form.arrival.data}\nDeparture: {form.departure.data}\n'
